1c6f336776fb5b7b2ee842d3abaa6bfc97f99c27
Alberth Perez <alberth.perez@hotmail.com>
2020-11-22 17:14:09 -0500

    Create project structure

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
+import React from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import Detail from './components/Detail';
+import Home from './pages/Home';
+import NotFound from './components/NotFound';
+import Layout from './components/Layout';
 import './App.css';
 
 function App() {
   return (
-    <div >
-      Hola mundo
-    </div>
+    <BrowserRouter>
+      {/* <ServerProvider> */}
+        <Layout>
+          <Switch>
+            <Route exact path='/movie/:id' component={Detail} />
+            <Route exact path='/' component={Home} />
+            <Route component={NotFound} />
+          </Switch>
+        </Layout>
+      {/* </ServerProvider> */}
+      </BrowserRouter>
   );
 }
 
